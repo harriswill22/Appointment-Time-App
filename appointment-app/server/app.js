7ec27f4d1c6186a3f4ed7f5ce344b13a72917435
Will Harris <Harriswill22@yahoo.com>
2019-01-24 11:59:39 -0500

    requires phone and name before updating time

--- appointment-app/server/app.js
+++ appointment-app/server/app.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
+const Model = require('./model');
 
 const app = express();
 const port = 7000;
+const model = new Model();
 
 // Allows your server to parse json data
 app.use(bodyParser.json());
@@ -14,6 +16,49 @@ app.use(bodyParser.urlencoded({
 app.get('/', (req, res) => {
     return res.status(200).json('Welcome to Appointment App API');
 });
+
+// gets all timeslots 
+app.get('/timeslots', (req, res) => {
+    return res.status(200).json(model.getTimeSlots());
+});
+
+// gets single time slot by id
+app.get('/timeslot/:id', (req, res) => {
+    if (!req.params.id) {
+        return res.status(400).json('id param is required');
+    }
+    const slotId = req.params.id;
+    const timeSlot = model.getTimeSlot(slotId);
+
+    // if time slot id not found return 404
+    if (!timeSlot) {
+        return res.status(404).json('Time Slot not found');
+    }
+
+    return res.status(200).json(timeSlot);
+});
+
+app.post('/timeslot/update/:id', (req, res) => {
+    if (!req.params.id) {
+        return res.status(400).json('id param is required');
+    }
+    if (!req.body.name || !req.body.phone) {
+        return res.status(400).json('Name and Phone number are required');
+    }
+
+    const slotId = req.params.id;
+    const name = req.body.name;
+    const phone = req.body.phone;
+
+    const timeSlotIsUpdated = model.updateTimeSlot(slotId, name, phone);
+    
+    if(!timeSlotIsUpdated) {
+        return res.status(404).json('Failed! Time slot not updated.')
+    }
+
+    return res.status(200).json('Success! Time slot updated.');
+
+})
 
 app.listen(port, (err) => {
     if (err) {

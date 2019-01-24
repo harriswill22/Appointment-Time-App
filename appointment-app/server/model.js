77f3651c8300cffe44f5144d517a2f65f51b346d
Will Harris <Harriswill22@yahoo.com>
2019-01-24 10:30:09 -0500

    creates Model class

---
+++ appointment-app/server/model.js
@@ -0,0 +1,93 @@
+import { get } from "https";
+
+class Model {
+    // Populates back-end with 9-5 
+    timeSlots = [
+       {
+           id: 1,
+           time: '9: AM',
+           name: '',
+           phone: ''
+       },
+       {
+        id: 2,
+        time: '10: AM',
+        name: '',
+        phone: ''
+    },
+    {
+        id: 3,
+        time: '11: AM',
+        name: '',
+        phone: ''
+    },
+    {
+        id: 4,
+        time: '12: AM',
+        name: '',
+        phone: ''
+    },
+    {
+        id: 5,
+        time: '1: PM',
+        name: '',
+        phone: ''
+    },
+    {
+        id: 6,
+        time: '2: PM',
+        name: '',
+        phone: ''
+    },
+    {
+        id: 7,
+        time: '3: PM',
+        name: '',
+        phone: ''
+    },
+    {
+        id: 8,
+        time: '4: PM',
+        name: '',
+        phone: ''
+    },
+    {
+        id: 9,
+        time: '5: PM',
+        name: '',
+        phone: ''
+    },
+    ]
+
+    getTimeSlots() {
+        return this.getTimeSlots;
+    }
+
+    getTimeSlot(slotId) {
+        // Find a single time slot by id from the array of timeslots
+        return this.timeSlots.find((timeSlot) => {
+            return timeSlot.id === slotId;
+        });
+
+    }
+
+    updateTimeSlot(slotId, name, phone) {
+        // Use to find slot index in timeSlots array
+        const slotIndex = this.timeSlots.findIndex((timeSlot) => {
+            return timeSlot.id === slotId;
+        });
+        // Updates name and phone
+        const timeSlotToUpdate = this.getTimeSlot(slotId);
+        const updatedTimeSlot = {
+            ...timeSlotToUpdate,
+            name: name,
+            phone: phone
+        };
+        
+        // removes one slot and replaces it with an updated timeSlot
+        this.timeSlots.splice(slotIndex, 1, updatedTimeSlot);
+        return this.timeSlots;
+        
+
+    }
+}
